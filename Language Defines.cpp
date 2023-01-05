da3ce9a23e25274d68481bbd7c1a69ad07f4b01f
Marco Antonio Jaguaribe Costa <marco.antonio.costa@gmail.com>
2023-01-05 06:11:23 -0300

    WIP: trying to bring some sanity to how language is set
    everything localization related being confined to something that's
    built in isolation and stuffs links to as needed would be an
    improvement, right now its preprocessor hell everywhere

--- Language Defines.cpp
+++ Language Defines.cpp
@@ -1,21 +1,35 @@
 #include "Language Defines.h"
 
-#if defined(ENGLISH)
-#	pragma message(" (Language set to ENGLISH, You'll need english CDs)")
-#elif defined(GERMAN)
-#	pragma message(" (Language set to GERMAN, You'll need Topware/german CDs)")
-#elif defined(RUSSIAN)
-#	pragma message(" (Language set to RUSSIAN, You'll need russian CDs)")
-#elif defined(DUTCH)
-#	pragma message(" (Language set to DUTCH, You'll need dutch CDs)")
-#elif defined(POLISH)
-#	pragma message(" (Language set to POLISH, You'll need polish CDs)")
-#elif defined(FRENCH)
-#	pragma message(" (Language set to FRENCH, You'll need french CDs)")
-#elif defined(ITALIAN)
-#	pragma message(" (Language set to ITALIAN, You'll need italian CDs)")
-#elif defined(CHINESE)
-#	pragma message(" (Language set to CHINESE, You'll need chinese CDs)")
-#else
-#	error "At least You have to specify a Language somewhere. See comments above."
+constexpr auto Language() -> std::string_view {
+#ifdef ENGLISH
+#pragma message(" (Language set to ENGLISH, You'll need english CDs)")
+	return "ENGLISH";
+#elif CHINESE
+#pragma message(" (Language set to CHINESE, You'll need chinese CDs)")
+	return "CHINESE";
+#elif DUTCH
+#pragma message(" (Language set to DUTCH, You'll need dutch CDs)")
+	return "DUTCH";
+#elif ITALIAN
+#pragma message(" (Language set to ITALIAN, You'll need italian CDs)")
+	return "ITALIAN";
+#elif RUSSIAN
+#pragma message(" (Language set to RUSSIAN, You'll need russian CDs)")
+	return "RUSSIAN";
+#elif GERMAN
+#pragma message(" (Language set to GERMAN, You'll need Topware/german CDs)")
+	return  "GERMAN";
+#elif FRENCH
+#pragma message(" (Language set to FRENCH, You'll need french CDs)")
+	return  "FRENCH";
+#elif POLISH
+#pragma message(" (Language set to POLISH, You'll need polish CDs)")
+	return "POLISH";
+#endif
+}
+
+// Tests
+#ifdef ENGLISH
+static_assert(Language() == "ENGLISH", "lang() must be able to be evaluated at compile-time");
+static_assert(Language().substr(0,2) == "EN", "lang().substr(...) must be able to be evaluated at compile-time");
 #endif
